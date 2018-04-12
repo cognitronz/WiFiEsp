2b31ffe0704b7da69c5af780fdc3a05aaaad0f39
TZ <cognitronz@gmail.com>
2018-04-12 09:41:32 -0700

    changed example to use Linduino one board

--- examples/WebClient/WebClient.ino
+++ examples/WebClient/WebClient.ino
@@ -7,16 +7,19 @@
  For more details see: http://yaab-arduino.blogspot.com/p/wifiesp-example-client.html
 */
 
+#include <SPI.h>
 #include "WiFiEsp.h"
+#include "lt_dc2259.h"
+#include "private.h"  // not part of repo
 
 // Emulate Serial1 on pins 6/7 if not present
 #ifndef HAVE_HWSERIAL1
 #include "SoftwareSerial.h"
-SoftwareSerial Serial1(6, 7); // RX, TX
+SoftwareSerial swSerial(2, 3); // RX, TX
 #endif
 
-char ssid[] = "Twim";            // your network SSID (name)
-char pass[] = "12345678";        // your network password
+char ssid[] = WIFI_SSID;            // your network SSID (name)
+char pass[] = WIFI_PASS;        // your network password
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
 char server[] = "arduino.cc";
@@ -24,14 +27,21 @@ char server[] = "arduino.cc";
 // Initialize the Ethernet client object
 WiFiEspClient client;
 
+cell_asic bms_ic[TOTAL_IC];
+
 void setup()
 {
   // initialize serial for debugging
   Serial.begin(115200);
-  // initialize serial for ESP module
-  Serial1.begin(9600);
+
+  quikeval_SPI_connect();
+  spi_enable(SPI_CLOCK_DIV16); // This will set the Linduino to have a 1MHz Clock
+  LTC681x_init_cfg(TOTAL_IC, bms_ic);
+  LTC6811_reset_crc_count(TOTAL_IC,bms_ic);
+  LTC6811_init_reg_limits(TOTAL_IC,bms_ic);
+
   // initialize ESP module
-  WiFi.init(&Serial1);
+  WiFi.init(&swSerial);
 
   // check for the presence of the shield
   if (WiFi.status() == WL_NO_SHIELD) {
@@ -49,8 +59,8 @@ void setup()
   }
 
   // you're connected now, so print out the data
-  Serial.println("You're connected to the network");
-  
+  Serial.println("You're connected to the network... wait 5 sec");
+  delay(5000);
   printWifiStatus();
 
   Serial.println();
@@ -58,11 +68,16 @@ void setup()
   // if you get a connection, report back via serial
   if (client.connect(server, 80)) {
     Serial.println("Connected to server");
+    Serial.println("Make HTTP request");
     // Make a HTTP request
     client.println("GET /asciilogo.txt HTTP/1.1");
     client.println("Host: arduino.cc");
     client.println("Connection: close");
     client.println();
+  }
+  else
+  {
+    Serial.println("Connection to server failed!");
   }
 }
 
